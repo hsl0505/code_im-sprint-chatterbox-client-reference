973d3b5c5851219e8a7f44748162e4d3ab76f5c4
Seung-Yoon Oh <feynubrick@gmail.com>
2019-08-05 13:21:26 +0900

    added room creation and selection functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,90 +1,176 @@
-// YOUR CODE HERE:
-
+/* eslint-disable no-console */
+// eslint-disable-next-line
 const app = {
-  server: 'http://52.78.206.149:3000/messages',
+  server: "http://52.78.213.9:3000/messages",
+  data: [],
+  rooms: [],
+  currentRoomIndex: 0,
+  username: "",
   init: () => {
-    app.addEventHandlers();
-    app.fetch(json => {
-      json.forEach(app.renderMessage);
-    });
-  },
-  fetchAndRender: () => {
-    app.fetch(data => {
-      data.forEach(app.renderMessage);
-    });
-  },
-  addEventHandlers: () => {
-    let submit = document.querySelector('#send .submit');
-    if (submit) {
-      submit.addEventListener('submit', app.handleSubmit);
-    }
+    app.fetch(app.updateRoomList);
+    setInterval(app.refreshChats, 1000);
+    var sendChatBtn = document.querySelector("#send-chat-btn");
+    sendChatBtn.onclick = app.sendChat;
+
+    var sendChatInput = document.querySelector("#chat-input");
+    sendChatInput.onkeydown = function(event) {
+      if (event.keyCode === 13) {
+        app.sendChat();
+      }
+    };
+
+    var usernameInput = document.querySelector("#username-input");
+    usernameInput.onclick = () => {
+      if (usernameInput.value === "") {
+        app.username = "guest";
+      } else {
+        app.username = usernameInput.value;
+      }
+      app.renderUserName();
+    };
+
+    var creatRoom = document.querySelector("#create-room-btn");
+    creatRoom.onclick = app.createRoom;
+
+    var select = document.querySelector("#room-select");
+    select.onchange = app.updateRoomList;
+
+    var changeUserNameBTN = document.querySelector("#change-user-name");
+    changeUserNameBTN.onclick = app.changeUserName;
   },
   fetch: callback => {
     window
-      .fetch(app.server)
-      .then(resp => {
-        return resp.json();
-      })
-      .then(callback);
-  },
-  send: (data, callback) => {
-    window
       .fetch(app.server, {
-        method: 'POST',
-        body: JSON.stringify(data),
+        method: "GET",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json"
         }
       })
-      .then(resp => {
-        return resp.json();
+      .then(response => {
+        return response.json();
       })
-      .then(callback);
+      .then(json => {
+        app.data = json;
+        if (callback !== undefined) {
+          callback();
+        }
+      });
+  },
+  send: message => {
+    window
+      .fetch(app.server, {
+        method: "POST",
+        body: JSON.stringify(message),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => {
+        return response.json();
+      });
   },
   clearMessages: () => {
-    document.querySelector('#chats').innerHTML = '';
+    document.querySelector("#chats").innerHTML = "";
   },
-  clearForm: () => {
-    document.querySelector('.inputUser').value = '';
-    document.querySelector('.inputChat').value = '';
-  },
-  renderMessage: ({ username, text, date, roomname }) => {
-    const tmpl = `<div class="chat">
-      <div class="username">${username
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')}</div>
-      <div>${text
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')}</div>
-      <div>${date}</div>
-      <div>${roomname
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')}</div>
-    </div>`;
+  renderMessage: message => {
+    var wrapper = document.createElement("div");
+    var usernameDiv = document.createElement("div");
+    var textDiv = document.createElement("div");
+    var roomnameDiv = document.createElement("div");
+    var dateDiv = document.createElement("div");
 
-    document.querySelector('#chats').innerHTML =
-      tmpl + document.querySelector('#chats').innerHTML;
+    wrapper.className = "chat";
+
+    document.querySelector("#chats").appendChild(wrapper);
+    wrapper.appendChild(usernameDiv);
+    wrapper.appendChild(textDiv);
+    wrapper.appendChild(roomnameDiv);
+    wrapper.appendChild(dateDiv);
+
+    usernameDiv.textContent = message.username;
+    textDiv.textContent = message.text;
+    roomnameDiv.textContent = message.roomname;
+    dateDiv.textContent = message.date;
+
+    usernameDiv.className = "username";
+    textDiv.className = "text";
+    roomnameDiv.className = "roomname";
+    dateDiv.className = "date";
   },
-  handleSubmit: e => {
-    e.preventDefault();
-    app.clearMessages();
-    app.send(
-      {
-        username: document.querySelector('.inputUser').value,
-        text: document.querySelector('.inputChat').value,
-        roomname: '코드스테이츠'
-      },
-      () => {
-        app.fetchAndRender();
-        app.clearForm();
+  updateRoomList: () => {
+    let select = document.querySelector("#room-select");
+    let index = select.selectedIndex;
+    if (index === -1) {
+      app.currentRoomIndex = 0;
+    } else {
+      app.currentRoomIndex = index;
+    }
+
+    for (let i = 0; i < app.data.length; i++) {
+      if (app.rooms.indexOf(app.data[i].roomname) === -1) {
+        app.rooms.push(app.data[i].roomname);
       }
-    );
+    }
+
+    select.textContent = "";
+    app.rooms.forEach((roomname, index) => {
+      let option = document.createElement("option");
+      option.value = roomname;
+      option.textContent = roomname;
+
+      if (index === app.currentRoomIndex) {
+        option.selected = true;
+      }
+      select.appendChild(option);
+    });
+  },
+  refreshChats: () => {
+    app.fetch();
+    document.querySelector("#chats").textContent = "";
+    var currentRoomname = document.querySelector("#room-select").value;
+
+    for (var i = app.data.length - 1; i >= 0; i--) {
+      if (app.data[i].roomname === currentRoomname) {
+        app.renderMessage(app.data[i]);
+      }
+    }
+  },
+  sendChat: (e, text) => {
+    if (text === undefined) {
+      text = document.querySelector("#chat-input").value;
+    }
+    var info = {
+      username: app.username,
+      text: text,
+      roomname: document.querySelector("#room-select").value
+    };
+    app.send(info);
+
+    document.querySelector("#chat-input").value = "";
+  },
+  createRoom: () => {
+    var roomName = document.querySelector("#roomname-input").value;
+    app.rooms.push(roomName.value);
+
+    var select = document.querySelector("#room-select");
+
+    let option = document.createElement("option");
+    option.value = roomName;
+    option.textContent = roomName;
+    option.selected = true;
+    select.appendChild(option);
+
+    app.currentRoomIndex = app.rooms.length - 1;
+    app.sendChat(null, "새로운 룸이 생성되었습니다");
+  },
+  renderUserName: () => {
+    var userName = document.querySelector("#user-name");
+
+    userName.textContent = `안녕하세요! ${app.username} 님`;
+  },
+  changeUserName: () => {
+    app.username = document.querySelector("#username-input").value;
+    app.renderUserName();
   }
 };
 
